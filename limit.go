6ea7ebdf44ba2e9deceb9390438845e5f0aa67ae
Joshua Liebow-Feeser <hello@synful.io>
2014-08-26 17:04:36 -0400

    Initial commit.

---
+++ limit.go
@@ -0,0 +1,176 @@
+package rate
+
+import (
+	"io"
+	"math"
+	"time"
+)
+
+const (
+	defaultQuantum = time.Millisecond * time.Duration(100)
+)
+
+type either interface {
+	io(p []byte) (n int, err error)
+}
+
+type eitherReader struct {
+	r io.Reader
+}
+
+func (e eitherReader) io(p []byte) (int, error) { return e.r.Read(p) }
+
+type eitherWriter struct {
+	w io.Writer
+}
+
+func (e eitherWriter) io(p []byte) (int, error) { return e.w.Write(p) }
+
+// limit implements the abstract functionality
+// common between writers and readers
+type limit struct {
+	e      either
+	writer bool
+
+	bps     uint64 // units per second
+	quantum time.Duration
+	bpq     int // units per quantum
+
+	t0   time.Time
+	left int
+}
+
+func newLimit(e either, writer bool, bps uint64, quantum time.Duration) limit {
+	if bps == 0 {
+		// Short-circuit so we don't divide by 0
+
+		// It only matters that bps and e are set
+		// so that Read calls with n > 0 will block
+		// forever.
+		return limit{e: e, bps: bps}
+	}
+	ret := limit{
+		e:       e,
+		writer:  writer,
+		bps:     bps,
+		quantum: quantum,
+		bpq:     (int(bps) * int(quantum)) / int(time.Second),
+	}
+	if ret.bpq == 0 {
+		ret.bpq = 1
+		ret.quantum = time.Second / time.Duration(ret.bps)
+	}
+	return ret
+}
+
+func (l *limit) io(p []byte) (n int, err error) {
+	if l.e == nil {
+		n, err = 0, io.EOF
+		return
+	}
+	if len(p) == 0 {
+		return
+	}
+	if l.bps == 0 {
+		time.Sleep(time.Duration(math.MaxInt16))
+	}
+
+	if l.left == 0 {
+		// If there are no bytes left in this quantum,
+		// wait until the next one.
+
+		// If l.t0 is the zero value of time.Time,
+		// (indicating that this is the first read)
+		// l.t0.Sub(time.Now()) < 0, and time.Sleep
+		// will return immediately.
+		time.Sleep(l.t0.Sub(time.Now()))
+		l.t0 = time.Now().Add(l.quantum)
+		l.left = l.bpq
+	}
+
+	buf := p
+	if len(p) > l.left {
+		// fmt.Fprintf(os.Stderr, "%v %v\n", len(p), l.left)
+		buf = p[:l.left]
+	}
+	n, err = l.e.io(buf)
+	l.left -= n
+	if l.writer && err == nil {
+		var ntmp int
+		ntmp, err = l.io(p[len(buf):])
+		n += ntmp
+	}
+	return
+}
+
+type limitReader struct {
+	l limit
+}
+
+func (l *limitReader) Read(p []byte) (n int, err error) {
+	n, err = l.l.io(p)
+	return
+}
+
+// NewLimitReader returns a new Reader that reads from
+// r at a maximum rate of bps bytes per second. If
+// bps == 0, any call to Read with len(p) > 0 will
+// sleep forever.
+func NewLimitReader(r io.Reader, bps uint64) io.Reader {
+	return NewLimitReaderQuantum(r, bps, defaultQuantum)
+}
+
+// NewLimitReaderQuantum creates a new Reader that
+// reads from r at a maximum rate of bps bytes
+// per second. If bps == 0, any call to Write with
+// len(p) > 0 will sleep forever.
+//
+// The returned Reader will attempt to control
+// the rate by limiting the number of bytes
+// read every quantum to bps * quantum. Smaller
+// values of quantum will make the rate more smooth
+// so long as calls to Read return quickly
+// enough and with enough data. However, if
+// quantum is too small, it may slow the rate
+// due to the overhead of many small read calls.
+// The default value (used by NewLimitReader) is 100ms.
+func NewLimitReaderQuantum(r io.Reader, bps uint64, quantum time.Duration) io.Reader {
+	return &limitReader{newLimit(eitherReader{r}, false, bps, quantum)}
+}
+
+type limitWriter struct {
+	l limit
+}
+
+func (l *limitWriter) Write(p []byte) (n int, err error) {
+	n, err = l.l.io(p)
+	return
+}
+
+// NewLimitWriter returns a new Writer that writes to w
+// at a maximum rate of bps bytes per second. If bps == 0,
+// any call to Write with len(p) > 0 will sleep forever.
+func NewLimitWriter(w io.Writer, bps uint64) io.Writer {
+	return NewLimitWriterQuantum(w, bps, defaultQuantum)
+}
+
+// NewLimitWriterQuantum creates a new Writer that
+// writes to w at a maximum rate of bps bytes
+// per second. If bps == 0, any call to Write with
+// len(p) > 0 will sleep forever.
+//
+// The returned Writer will attempt to control
+// the rate by limiting the number of bytes
+// written every quantum to bps * quantum
+// (if Write is called with len(p) > bpq * quantum,
+// it will write bpq * quantum and sleep repeatedly
+// until all of the bytes have been written). Smaller
+// values of quantum will make the rate more smooth
+// so long as calls to Write return quickly
+// enough and with enough data. However, if
+// quantum is too small, it may slow the rate
+// due to the overhead of many small read calls.
+// The default value (used by NewLimitWriter) is 100ms.
+func NewLimitWriterQuantum(w io.Writer, bps uint64, quantum time.Duration) io.Writer {
+	return &limitWriter{newLimit(eitherWriter{w}, true, bps, quantum)}
+}
---
+++ monitor.go
@@ -0,0 +1,267 @@
+// Copyright 2014 The Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+package rate
+
+import (
+	"io"
+	"sync/atomic"
+	"time"
+)
+
+const (
+	defaultPeroid = time.Duration(500) * time.Millisecond
+)
+
+type Rate struct {
+	Total uint64
+	Rate  float64
+}
+
+// A Monitor monitors the rate at which abstract events
+// happen (such as bytes written to an input stream).
+// Calling Add(n) signals that n events have happened.
+// Every period, the average rate at which events happened
+// over the preceding period and the total number of events
+// so far are either written to a channel or given as the
+// argument to a function.
+type Monitor struct {
+	f      func(r Rate)
+	period time.Duration
+	t0     time.Time
+	n, nn  uint64
+	exit   chan struct{}
+}
+
+// MakeMonitor creates a new Monitor which writes
+// the rate and total to the returned channel every period.
+// If period == 0, the default period of 500ms will
+// be used.
+func MakeMonitor(period time.Duration) (*Monitor, <-chan Rate) {
+	rch := make(chan Rate, 8)
+	return MakeMonitorFunc(period, func(r Rate) {
+		rch <- r
+	}), rch
+}
+
+// MakeMonitorFunc creates a new Monitor which calls f
+// in a separate goroutine every period. If period
+// == 0, the default period of 500ms will be used.
+func MakeMonitorFunc(period time.Duration, f func(r Rate)) *Monitor {
+	if period == 0 {
+		period = defaultPeroid
+	}
+	ret := &Monitor{
+		f:      f,
+		period: period,
+		exit:   make(chan struct{}, 1),
+	}
+	go ret.monitor()
+	return ret
+}
+
+func (m *Monitor) monitor() {
+	m.t0 = time.Now()
+	for {
+		select {
+		case <-m.exit:
+			return
+		default:
+			// Use default and sleep instead of
+			// a time.After case because extra
+			// thread switching under heavy loads
+			// makes a big performance difference.
+			time.Sleep(m.period)
+
+			// In case we missed an exit command
+			// while we were sleeping; this technically
+			// wouldn't invalidate the semantics,
+			// but it'd still be dumb to unnecessarily
+			// be doing stuff hundreds of milliseconds
+			// after we were told to stop.
+			select {
+			case <-m.exit:
+				return
+			default:
+			}
+
+			t1 := time.Now()
+			delta := t1.Sub(m.t0)
+			m.t0 = t1
+
+			nn := atomic.SwapUint64(&m.nn, 0)
+			m.n += nn
+
+			rate := float64(nn) / delta.Seconds()
+			m.f(Rate{m.n, rate})
+		}
+	}
+}
+
+func (m *Monitor) Add(n uint64) {
+	atomic.AddUint64(&m.nn, n)
+}
+
+// Close stops m from monitoring its rate. If m was
+// created with MakeMonitor, no more values will be
+// written to the channel, and if it was created with
+// MakeMonitorFunc, f will not be called again.
+func (m *Monitor) Close() {
+	// Since m.exit is buffered, the first
+	// value will always be sent. This way,
+	// subsequent calls to Close will never
+	// block.
+	select {
+	case m.exit <- struct{}{}:
+	default:
+	}
+}
+
+// A MonitorReader wraps an io.Reader and monitors the rate
+// at which bytes are read from it. Every period, the average
+// rate at which bytes were read over the preceding period
+// and the total number of bytes read so far are either written
+// to a channel, or passed as the argument to a function.
+type MonitorReader struct {
+	r   io.Reader
+	m   *Monitor
+	err error
+}
+
+// MakeMonitorReader creates a new MonitorReader which writes
+// the rate and total to the returned channel every period.
+// If period == 0, the default period of 500ms will be used.
+func MakeMonitorReader(r io.Reader, period time.Duration) (*MonitorReader, <-chan Rate) {
+	m, rch := MakeMonitor(period)
+	return &MonitorReader{r: r, m: m}, rch
+}
+
+// MakeMonitorReaderFunc creates a new MonitorReader which calls
+// f in a separate goroutine every period. If period == 0, the
+// default period of 500ms will be used.
+func MakeMonitorReaderFunc(r io.Reader, period time.Duration, f func(r Rate)) *MonitorReader {
+	m := MakeMonitorFunc(period, f)
+	return &MonitorReader{r: r, m: m}
+}
+
+func (m *MonitorReader) Read(p []byte) (n int, err error) {
+	if m.err != nil {
+		n, err = 0, m.err
+		return
+	}
+	if m.r == nil {
+		n, err = 0, io.EOF
+		return
+	}
+
+	n, err = m.r.Read(p)
+	m.m.Add(uint64(n))
+	return
+}
+
+// Close closes the reader; all subsequent calls to Read
+// will return io.EOF or any error previously encountered,
+// and the rate will not be reported any more. Additionally,
+// if m's underlying Reader implements the io.ReadCloser
+// interface, its Close method will be called, and its
+// return value will be returned from this method.
+//
+// If m's underlying writer implements io.ReadCloser,
+// but it's undesirable for its Close method to be called,
+// wrap it in a ReaderOnly before creating m.
+func (m *MonitorReader) Close() error {
+	m.m.Close()
+	defer func() { m.r = nil }()
+	if rc, ok := m.r.(io.ReadCloser); ok {
+		return rc.Close()
+	}
+	return nil
+}
+
+// A MonitorWriter wraps an io.Writer and monitors the rate
+// at which bytes are written to it, Every period, the average
+// rate at which bytes were written over the preceding period
+// and the total number of bytes written so far are either
+// written to a channel, or passed as the argument to a function.
+type MonitorWriter struct {
+	w   io.Writer
+	m   *Monitor
+	err error
+}
+
+// MakeMonitorWriter creates a new MonitorWriter which writes
+// the rate and total to the returned channel every period.
+// If period == 0, the default period of 500ms will be used.
+func MakeMonitorWriter(w io.Writer, period time.Duration) (*MonitorWriter, <-chan Rate) {
+	m, rch := MakeMonitor(period)
+	return &MonitorWriter{w: w, m: m}, rch
+}
+
+// MakeMonitorWriterFunc creates a new MonitorWriter which calls
+// f in a separate goroutine every period. If period == 0, the
+// default period of 500ms will be used.
+func MakeMonitorWriterFunc(w io.Writer, period time.Duration, f func(r Rate)) *MonitorWriter {
+	m := MakeMonitorFunc(period, f)
+	return &MonitorWriter{w: w, m: m}
+}
+
+func (m *MonitorWriter) Write(p []byte) (n int, err error) {
+	if m.err != nil {
+		n, err = 0, m.err
+		return
+	}
+	if m.w == nil {
+		n, err = 0, io.EOF
+		return
+	}
+
+	n, err = m.w.Write(p)
+	m.m.Add(uint64(n))
+	return
+}
+
+// Close closes the writer; all subsequent calls to Write
+// will return io.EOF or any error previously encountered,
+// and the rate will not be reported any more. Additionally,
+// if m's underlying Writer implements the io.WriteCloser
+// interface, its Close method will be called, and its
+// return value will be returned from this method.
+//
+// If m's underlying writer implements io.WriteCloser,
+// but it's undesirable for its Close method to be called,
+// wrap it in a WriterOnly before creating m.
+func (m *MonitorWriter) Close() error {
+	m.m.Close()
+	defer func() { m.w = nil }()
+	if wc, ok := m.w.(io.WriteCloser); ok {
+		return wc.Close()
+	}
+	return nil
+}
+
+// ReaderOnly allows a type which implements
+// more than just the io.Reader interface to
+// appear as though it only implements
+// io.Reader
+type ReaderOnly struct {
+	io.Reader
+}
+
+func (r ReaderOnly) Read(p []byte) (n int, err error) {
+	n, err = r.Reader.Read(p)
+	return
+}
+
+// WriterOnly allows a type which implements
+// more than just the io.Writer interface to
+// appear as though it only implements
+// io.Writer
+type WriterOnly struct {
+	io.Writer
+}
+
+func (w WriterOnly) Write(p []byte) (n int, err error) {
+	n, err = w.Writer.Write(p)
+	return
+}
